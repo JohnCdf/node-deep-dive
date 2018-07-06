638456e33254fc72f4619fa1aca53e1a841dc046
John S <diaz.john312@gmail.com>
2018-07-05 20:12:56 -0400

    Added redis caching for requests
    Some monkey patching was made on the mongoose.query, so that on every query execution, we could look into the redis cache and determine wether the same query had been made before. If not, save in the cache the result of this query for the next time

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ process.env.UV_THREADPOOL_SIZE = 2; // see thread.js
 const crypto = require('crypto');
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hey')
-})
+});
+
 app.get('/fast', (req, res) => {
   res.send('Fiumm')
-})
+});
+
 app.listen(3000);
